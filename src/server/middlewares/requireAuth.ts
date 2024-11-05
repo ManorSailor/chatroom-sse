ad50686707d521bffe04f27f0ca6ceb1214979f6
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2024-11-05 01:58:16 -0900

    feat(middleware): Attach JWT payload to request object for downstream access
    Type errors will be fixed later.

--- src/server/middlewares/requireAuth.ts
+++ src/server/middlewares/requireAuth.ts
@@ -6,13 +6,14 @@ const { SECRET_KEY = "test-secret" } = process.env;
 function verifyToken(req: Request, res: Response, next: NextFunction) {
   const token = req.headers.authorization?.replace("Bearer ", "");
 
-  jwt.verify(token ?? "", SECRET_KEY, (err, _) => {
+  jwt.verify(token ?? "", SECRET_KEY, (err, user) => {
     if (err) {
       res.status(403).send({
         message: "Unauthorized user. Access denied.",
       });
     }
 
+    req.user = user;
     next();
   });
 }
